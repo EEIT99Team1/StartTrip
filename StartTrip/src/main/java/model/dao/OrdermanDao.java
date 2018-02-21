da7e6d001c7c80147ef16f55cc49c9c4bb9a6874
林千雅 <lin@eeit99>
2018-02-21 17:03:01 +0800

    DAO位置錯誤，已修正!

--- StartTrip/src/main/java/model/dao/OrdermanDao.java
+++ StartTrip/src/main/java/model/dao/OrdermanDao.java
@@ -1,13 +1,33 @@
 package model.dao;
+import javax.transaction.Transactional;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import model.bean.OrdermanBean;
+
+@Repository
 public class OrdermanDao {
-
+	@Autowired
 	private SessionFactory sessionFactory;
-	
+
 	private Session getSession() {
 		return sessionFactory.getCurrentSession();
 	}
-	public OrdermanDao get()
+
+	@Transactional
+	public OrdermanBean get(int orderID) {
+		return getSession().get(OrdermanBean.class, orderID);
+	}
+
+	@Transactional
+	public OrdermanBean insert(OrdermanBean bean) {
+		if (bean != null) {
+			getSession().save(bean);
+			return bean;
+		}
+		return null;
+	}
 }
