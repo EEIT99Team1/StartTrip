8c9422ad078cff1b648556451dcb9a4ceb4865a7
年致遠 <ken666@yahoo.com.tw>
2018-02-22 21:32:02 +0800

    CustomerDao 施工中未完成

--- StartTrip/src/main/java/model/dao/CustomerDao.java
+++ StartTrip/src/main/java/model/dao/CustomerDao.java
@@ -1,5 +1,39 @@
 package model.dao;
 
-public class CustomerDao {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
 
+import model.bean.CustomerBean;
+
+public class CustomerDao {
+	private SessionFactory sessionFactory;
+	
+	private Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+	@Transactional
+	public CustomerBean select(String email) {
+		return getSession().get(CustomerBean.class, email);
+	}//select
+	
+	@Transactional
+	public CustomerBean insert(CustomerBean bean) {
+		if(bean!=null) {
+			getSession().save(bean);
+			return bean;
+		}
+		return null;
+	}//insert
+	
+	@Transactional
+	public boolean update() {
+		
+	}//update
+	
+	@Transactional
+	public boolean delete() {
+		
+	}//delete
+	
 }
