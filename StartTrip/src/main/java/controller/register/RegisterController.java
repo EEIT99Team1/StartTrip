c361e9e99f303603dabcbd816dc18d302bb29f53
蕭竹佐 <andycute8273@gmail.com>
2018-03-10 10:47:40 +0800

    寫好一般註冊的jsp & Register
    By HsiaoChuTso

--- StartTrip/src/main/java/controller/register/RegisterController.java
+++ StartTrip/src/main/java/controller/register/RegisterController.java
@@ -1,18 +1,77 @@
 package controller.register;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import model.bean.CustomerBean;
+import model.dao.CustomerDao;
 
 @Controller
 @RequestMapping(path="/Register.controller")
 public class RegisterController {
+	
+	@Autowired
+	private CustomerDao dao;
+	
 	@RequestMapping(method= {RequestMethod.POST},produces="text/plain;charset=utf-8")
-	public String regist(CustomerBean bean) {
+	public String regist(CustomerBean bean , Model model) {
 		
-		System.out.println(bean);
-		return null;
+		Map<String,String> errorMsg = new HashMap<String,String>();
+		Map<String,String> successMsg = new HashMap<String,String>(); 
+		
+		model.addAttribute("error",errorMsg);
+		model.addAttribute("success",successMsg);
+		
+			if(bean.getEmail()==null || bean.getEmail().trim().length()==0) {
+				errorMsg.put("errorIDEmpty", "帳號欄必須輸入");
+			}
+			if(bean.getPassword()==null || bean.getPassword().trim().length()==0) {
+				errorMsg.put("errorPasswordEmpty", "密碼欄必須輸入");
+			}
+			if(bean.getLastname()==null || bean.getLastname().trim().length()==0) {
+				errorMsg.put("errorLastname", "姓氏欄必須輸入");
+			}
+			if(bean.getFirstname()==null || bean.getFirstname().trim().length()==0) {
+				errorMsg.put("errorFirstname", "名字欄必須輸入");
+			}
+			if(bean.getCountry()==null || bean.getCountry().trim().length()==0) {
+				errorMsg.put("errorCountry", "國家欄必須輸入");
+			}
+			if(bean.getBirthday()==null || bean.getBirthday().trim().length()==0) {
+				errorMsg.put("errorBirthday", "生日欄必須輸入");
+			}
+			if(bean.getPhonenumber()==null || bean.getPhonenumber().trim().length()==0) {
+				errorMsg.put("errorPhonenumber", "手機號碼欄必須輸入");
+			}
+			
+			if(!errorMsg.isEmpty() && errorMsg!=null) {
+				System.out.println("失敗");
+				return "register.error";
+			}
+		//判斷是否帳號已存在
+		if(!(dao.select(bean.getEmail())==null)){
+			errorMsg.put("errorIDDup", "此帳號已存在，請換一個帳號");
+			System.out.println("errorIDDup = "+dao.select(bean.getEmail()));
+		}else {
+			successMsg.put("successIDDup", "此帳號可使用");
+			System.out.println("successIDDup ="+dao.select(bean.getEmail()));
+		}
+		
+		if(!errorMsg.isEmpty() && errorMsg!=null) {
+			System.out.println("失敗");
+			return "register.error";
+		}else{
+			successMsg.put("InsertOK", "恭喜您註冊成功。");
+			bean.setBonus(0);
+			dao.insert(bean);
+		}
+		System.out.println("成功");
+		return "register.success";
 	}
 }
--- StartTrip/src/main/webapp/page/register/Register.jsp
+++ StartTrip/src/main/webapp/page/register/Register.jsp
@@ -8,31 +8,40 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<form action="<c:url value='/Register.controller'/>" method="post">
+	<form action="<c:url value='/Register.controller'/>"method="post">
 		<div>
 			<label for="email">Email:</label> <input type="text" name="email" size="25" />
+			&nbsp;<small><Font color='red'  size="-3">${error.errorIDEmpty}</Font></small>
+			&nbsp;<small><Font color='red'  size="-3">${success.successIDDup}</Font></small>
 		</div>
 		<div>
 			<div>
 				<label for="passid">密碼:</label> <input type="password" name="password" size="25" />
+				&nbsp;<small><Font color='red'  size="-3">${error.errorPasswordEmpty}</Font></small>
 			</div>
 			<label for="lastname">姓:</label> <input type="text" name="lastname" size="25" />
+			&nbsp;<small><Font color='red'  size="-3">${error.errorLastname}</Font></small>
 		</div>
 		<div>
 			<label for="firstname">名:</label> <input type="text" name="firstname" size="25" />
+			&nbsp;<small><Font color='red'  size="-3">${error.errorFirstname}</Font></small>
 		</div>
 		<div>
-			<label for="country">Country:</label> <select name="country"></select>
+			<label for="country">Country:</label> <input type="text" name="country" size="25" />
+			&nbsp;<small><Font color='red'  size="-3">${error.errorCountry}</Font></small>
 		</div>
 		<div>
 			<label for="birthday">生日:</label> <input type="text" name="birthday" size="25" />
+			&nbsp;<small><Font color='red'  size="-3">${error.errorBirthday}</Font></small>
 		</div>
 		<div>
 			<label for="phonenumber">手機號碼:</label> <input type="text" name="phonenumber" size="25" />
+			&nbsp;<small><Font color='red'  size="-3">${error.errorPhonenumber}</Font></small>
 		</div>
 		<div>
 			<input type="submit" name="submit" value="註冊一般會員" />
 		</div>
+		<h1>${error.errorIDDup}</h1>
 	</form>
 </body>
 </html>
